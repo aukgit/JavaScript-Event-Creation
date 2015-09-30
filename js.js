a87720b61d63580e449279cf4fc27167c6306676
Alim Ul Karim <devorg.bd@gmail.com>
2015-09-30 18:38:27 +0600

    initial push
    initial push

---
+++ js.js
@@ -0,0 +1,38 @@
+﻿/// <reference path="jquery-2.1.4.intellisense.js" />
+$(function () {
+    var SpecialEvent = new CustomEvent(
+  "SpecialMessage",
+  {
+      detail:
+      {
+          message: "Hello There",
+          time: new Date()
+      },
+      bubbles: true,
+      cancelable: true
+  });
+    var binder = document.getElementById("event-binder");
+
+    // Assign an event to the object.
+    binder.addEventListener("SpecialMessage", HandleEvent, false);
+    var $binder = $(binder);
+    $binder.on("click", function () {
+        SpecialEvent.detail.message = "new message";
+        binder.dispatchEvent(SpecialEvent);
+    });
+
+    $binder.on("SpecialMessage", function (e) {
+        console.log("Hello World!");
+        console.log(e);
+    });
+
+    function HandleEvent(event) {
+        // Display the event information.
+        document.getElementById("display").innerHTML =
+         "Control: " + event.currentTarget.id +
+         "<br />Message: " + event.detail.message +
+         "<br />Time sent: " +
+         event.detail.time.toTimeString();
+        return false;
+    }
+});
